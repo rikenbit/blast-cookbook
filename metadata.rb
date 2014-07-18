35022fce004d34f1de5e45d2a944883d529fb7ca
Manabu ISHII <manabu.ishii.rb@gmail.com>
2014-07-18 17:07:52 +0900

    bug fix: check already intalled

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,2 @@
 name "blast-cookbook"
-version "0.1.0"
+version "0.1.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,7 @@ when 'centos'
   execute "install blast" do
     command "yum -y install ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-2.2.29+-1.x86_64.rpm"
     action :run
+    not_if { File.exists?("/usr/bin/blastp") }
   end
 end
 
